18fb2e2f1b271a41324488bfe0687446a9b3d2ce
kulesz88 <kulesz88@users.noreply.github.com>
2019-02-10 13:30:30 +0100

    Lower case test added

--- test/test_formater.py
+++ test/test_formater.py
@@ -1,9 +1,17 @@
 from hello_world.formater import plain_text_upper_case
+from hello_world.formater import plain_text_lower_case
 import unittest
 
 
 class TestFormater(unittest.TestCase):
     def test_plain_lowercase(self):
+        r = plain_text_lower_case("wwimie", "eeemsg")
+        name = r.split(" ")[0]
+        msg = r.split(" ")[1]
+        self.assertTrue(name.islower())
+        self.assertTrue(msg.islower())
+
+    def test_plain_uppercase(self):
         r = plain_text_upper_case("WWIMIE", "EEEMSG")
         name = r.split(" ")[0]
         msg = r.split(" ")[1]
